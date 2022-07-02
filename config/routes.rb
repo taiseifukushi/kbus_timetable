c4b09d281a209b3bda7989150b3c858186224c48
husita-h <taisei.fukushi1129@gmail.com>
2022-07-02 21:33:11 +0900

    ルーティングの設定を追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   root "base_apis#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # resource "stations"
-  #   /search" to: "stations#search"
+  post 'search', to: 'base_apis#search'
 end
