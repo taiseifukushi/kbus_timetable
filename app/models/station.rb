fb892ee906bcd03ccb44e435127f91d614e2bcbc
husita-h <taisei.fukushi1129@gmail.com>
2022-07-05 00:41:47 +0900

    rubocop -a

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,78 +1,78 @@
 class Station < ApplicationRecord
-    has_many :jikan
+  has_many :jikan
 
-    OJI_ROUTE = [
-        "JR王子駅1",
-        "王子本町交番",
-        "障害者福祉センター",
-        "中央図書館",
-        "王子アパート",
-        "紅葉橋",
-        "北区役所",
-        "飛鳥山公園1",
-        "一里塚1",
-        "花と森の東京病院1",
-        "旧古河庭園1",
-        "滝野川小学校1",
-        "霜降橋1",
-        "JR駒込駅",
-        "霜降橋2",
-        "滝野川小学校2",
-        "旧古河庭園2",
-        "花と森の東京病院2",
-        "一里塚2",
-        "飛鳥山公園2",
-        "JR王子駅2",
-    ].freeze
+  OJI_ROUTE = %w[
+    JR王子駅1
+    王子本町交番
+    障害者福祉センター
+    中央図書館
+    王子アパート
+    紅葉橋
+    北区役所
+    飛鳥山公園1
+    一里塚1
+    花と森の東京病院1
+    旧古河庭園1
+    滝野川小学校1
+    霜降橋1
+    JR駒込駅
+    霜降橋2
+    滝野川小学校2
+    旧古河庭園2
+    花と森の東京病院2
+    一里塚2
+    飛鳥山公園2
+    JR王子駅2
+  ].freeze
 
-    TABATA_ROUTE = [
-        "JR駒込駅",
-        "駒込一丁目",
-        "田端三丁目",
-        "田端区民センター",
-        "田端二丁目",
-        "JR田端駅",
-        "田端五丁目",
-        "富士見橋エコー広場館",
-        "中里保育園",
-        "女子聖学院",
-        "滝野川会館",
-        "滝野川小学校",
-        "霜降橋",
-        "JR駒込駅",
-    ].freeze
+  TABATA_ROUTE = %w[
+    JR駒込駅
+    駒込一丁目
+    田端三丁目
+    田端区民センター
+    田端二丁目
+    JR田端駅
+    田端五丁目
+    富士見橋エコー広場館
+    中里保育園
+    女子聖学院
+    滝野川会館
+    滝野川小学校
+    霜降橋
+    JR駒込駅
+  ].freeze
 
-    RELAY_POINT = {
-        up: [
-            "JR駒込駅",
-            "JR駒込駅"
-        ],
-        down: [
-            "滝野川会館",
-            "旧古河庭園2"
-        ]
-    }.freeze
+  RELAY_POINT = {
+    up: %w[
+      JR駒込駅
+      JR駒込駅
+    ],
+    down: %w[
+      滝野川会館
+      旧古河庭園2
+    ]
+  }.freeze
 
-    def self.calculate_wait_time(params)
-        get_on_station_id = Station.find_by(id: params[:get_on])[:id]
-        if norikae_necessary?(params[:get_on], params[:get_off])
-            up = { up: up?(get_on) }
-            result = Jikan.call_calculate_wait_time(get_on_station_id, up)
-        else
-            result = Jikan.non_wait_time(get_on_station_id)
-        end
+  def self.calculate_wait_time(params)
+    get_on_station_id = Station.find_by(name: params[:get_on])&.id
+    if norikae_necessary?(params[:get_on], params[:get_off])
+      up = { up: up?(get_on) }
+      result = Jikan.call_calculate_wait_time(get_on_station_id, up)
+    else
+      result = Jikan.non_wait_time(get_on_station_id)
     end
+  end
 
-    private
+  private
 
-    def norikae_necessary?(get_on, get_off)
-        arg1 = OJI_ROUTE.include?(get_on) ? true : false
-        arg2 = OJI_ROUTE.include?(get_off) ? true : false
-        arg1 && arg2 ? false : true
-    end
+  def norikae_necessary?(get_on, get_off)
+    arg1 = OJI_ROUTE.include?(get_on) ? true : false
+    arg2 = OJI_ROUTE.include?(get_off) ? true : false
+    arg1 && arg2 ? false : true
+  end
 
-    def up?(get_on)
-        arg1 = OJI_ROUTE.include?(get_on) ? true : false
-        is_up = arg1 ? true : false
-    end
+  def up?(get_on)
+    arg1 = OJI_ROUTE.include?(get_on) ? true : false
+    is_up = arg1 ? true : false
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,83 +1,83 @@
 OJI_ROUTE = [
-    ["JR王子駅1", false],
-    ["王子本町交番", false],
-    ["障害者福祉センター", false],
-    ["中央図書館", false],
-    ["王子アパート", false],
-    ["紅葉橋", false],
-    ["北区役所", false],
-    ["飛鳥山公園1", false],
-    ["一里塚1", false],
-    ["花と森の東京病院1", false],
-    ["旧古河庭園1", false],
-    ["滝野川小学校1", false],
-    ["霜降橋1", false],
-    ["JR駒込駅", true],
-    ["霜降橋2", false],
-    ["滝野川小学校2", false],
-    ["旧古河庭園2", true],
-    ["花と森の東京病院2", false],
-    ["一里塚2", false],
-    ["飛鳥山公園2", false],
-    ["JR王子駅2", false],
+  ['JR王子駅1', false],
+  ['王子本町交番', false],
+  ['障害者福祉センター', false],
+  ['中央図書館', false],
+  ['王子アパート', false],
+  ['紅葉橋', false],
+  ['北区役所', false],
+  ['飛鳥山公園1', false],
+  ['一里塚1', false],
+  ['花と森の東京病院1', false],
+  ['旧古河庭園1', false],
+  ['滝野川小学校1', false],
+  ['霜降橋1', false],
+  ['JR駒込駅', true],
+  ['霜降橋2', false],
+  ['滝野川小学校2', false],
+  ['旧古河庭園2', true],
+  ['花と森の東京病院2', false],
+  ['一里塚2', false],
+  ['飛鳥山公園2', false],
+  ['JR王子駅2', false]
 ]
 
 TABATA_ROUTE = [
-    ["JR駒込駅", true],
-    ["駒込一丁目", false],
-    ["田端三丁目", false],
-    ["田端区民センター", false],
-    ["田端二丁目", false],
-    ["JR田端駅", false],
-    ["田端五丁目", false],
-    ["富士見橋エコー広場館", false],
-    ["中里保育園", false],
-    ["女子聖学院", false],
-    ["滝野川会館", true],
-    ["滝野川小学校", false],
-    ["霜降橋", false],
+  ['JR駒込駅', true],
+  ['駒込一丁目', false],
+  ['田端三丁目', false],
+  ['田端区民センター', false],
+  ['田端二丁目', false],
+  ['JR田端駅', false],
+  ['田端五丁目', false],
+  ['富士見橋エコー広場館', false],
+  ['中里保育園', false],
+  ['女子聖学院', false],
+  ['滝野川会館', true],
+  ['滝野川小学校', false],
+  ['霜降橋', false]
 ]
 
 MAIJI_LIST_OJI = [
-    [15, 35, 55],
-    [16, 36, 55]
+  [15, 35, 55],
+  [16, 36, 55]
 ]
 
 MAIJI_LIST_TABATA = [
-    [15, 35, 55],
-    [16, 36, 55]
+  [15, 35, 55],
+  [16, 36, 55]
 ]
 
 SIHATU_HOUR = 7
 SAISHUU_HOUR = 19
 
 def create_jkokuhyo(staions, maiji_list)
-    maiji_list.each_with_index do |maiji, index|
-        # [15, 35, 55]
-        maiji.each_with_index do |min, idx|
-            # min 15
-            repeat_counts.times do |count|
-                staions.each_with_index do |station, i|
-                    oji_station = Oji.create(
-                        name: station[0],
-                        is_relay_point: station[1]
-                    )
-                    at_time = count + 6
-                    Jikan.create(
-                        station_id: oji_station[:id],
-                        order: idx,
-                        get_on_time_hour: at_time,
-                        get_on_time_minute: min,
-                        row: count
-                    )
-                end
-            end
+  maiji_list.each_with_index do |maiji, _index|
+    # [15, 35, 55]
+    maiji.each_with_index do |min, idx|
+      # min 15
+      repeat_counts.times do |count|
+        staions.each_with_index do |station, _i|
+          oji_station = Oji.create(
+            name: station[0],
+            is_relay_point: station[1]
+          )
+          at_time = count + 6
+          Jikan.create(
+            station_id: oji_station[:id],
+            order: idx,
+            get_on_time_hour: at_time,
+            get_on_time_minute: min,
+            row: count
+          )
         end
+      end
     end
+  end
 end
 
 def repeat_counts
-    SAISHUU_HOUR - SIHATU_HOUR + 1
+  SAISHUU_HOUR - SIHATU_HOUR + 1
 end
 
 create_jkokuhyo(OJI_ROUTE, MAIJI_LIST_OJI)
