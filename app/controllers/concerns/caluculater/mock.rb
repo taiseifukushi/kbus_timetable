74179273f268a75103b49147f9a3a8a50b627769
husita-h <taisei.fukushi1129@gmail.com>
2022-08-13 17:28:10 +0900

    モジュールを分割 #15

--- app/controllers/concerns/caluculater/mock.rb
+++ app/controllers/concerns/caluculater/mock.rb
@@ -1,24 +1,53 @@
 module Caluculater
   module Mock
 
-    # mockの結果
-    def mock_result_hash_index
-      { on: nil, off: nil, wait_time: nil }
-    end
+    # def formate_current_time
+    #   current_time.strftime("%m月/%d日 %H時:%M分:%S秒")
+    # end
 
-    # mockの結果
-    def test_result_struct_index
-      @caluculated = result_struct(nil, mock_result_hash_index)
-    end
+    # def current_time
+    #   Time.zone.now
+    # end
 
-    # mockの結果
-    def mock_result_hash
-      { on: "Aバス", off: "Bバス", wait_time: 10 }
-    end
+    # def mock_hash
+    #   {
+    #     on: 'Aバス',
+    #     off: 'Bバス',
+    #     relay_point: 10,
+    #     wait_time: 10
+    #   }
+    # end
 
-    def test_result_struct
-      @caluculated = result_struct(current_time, mock_result_hash)
-    end
+    # def result_hash
+    #   {
+    #     on: '',
+    #     off: 'Bバス',
+    #     relay_point: 10,
+    #     wait_time: 10
+    #   }
+    # end
+
+    # # mockの結果
+    # def mock_result_hash_index
+    #   { on: nil, off: nil, wait_time: nil }
+    # end
+
+    # def test_result_struct_index
+    #   result_struct(nil, mock_result_hash_index)
+    # end
+
+    # def mock_result_hash
+    #   { on: "Aバス", off: "Bバス", relay_point: "Cバス", wait_time: 10 }
+    # end
+
+    # def test_result_struct
+    #   caluculation_result_struct(current_time: formate_current_time, mock_result_hash: mock_result_hash)
+    # end
+
+    # def caluculation_result_struct(current_time: time, mock_result_hash: hash)
+    #   caluculator = Struct.new(:now, :on, :off, :relay_point, :wait_time)
+    #   caluculator.new(time, hash[:on], hash[:off], hash[:wait_time], hash[:wait_time])
+    # end
   end
 end
 
