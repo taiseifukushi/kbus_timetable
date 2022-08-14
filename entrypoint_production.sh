f913ab56980d2b7ce45f65ddbaa48c3497b7ed8b
husita-h <taisei.fukushi1129@gmail.com>
2022-08-15 00:28:33 +0900

    [fix]デプロイ時pushするDockerfileを変更 #6

--- entrypoint_production.sh
+++ entrypoint_production.sh
@@ -7,7 +7,3 @@ heroku config:set RAILS_ENV=production
 
 # production環境のDBを作成する
 RAILS_ENV=production bundle exec rails db:drop db:create db:migrate db:seed
-
-# pumaを使ってサーバーを起動
-# https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#config
-RAILS_ENV=production bundle exec puma -C config/puma.rb
