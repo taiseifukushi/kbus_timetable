8b29465442e9be7e84b10c41722757a3c6669440
husita-h <taisei.fukushi1129@gmail.com>
2022-09-11 12:56:23 +0900

    config.require_master_keyをtrueにする

--- start_production.sh
+++ start_production.sh
@@ -5,5 +5,5 @@ rm -f /rails_app/tmp/pids/server.pid
 
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
-bundle exec rails db:migrate RAILS_ENV=production
-bundle exec rails server -b 0.0.0.0 RAILS_ENV=production
+bundle exec rails db:create db:migrate
+bundle exec rails server -b 0.0.0.0
